757a9a0a503e8d7109d82ca23abc3fff1bc7c190
Maximilian Huber <gh@maxhbr.de>
2021-03-31 19:59:10 +0200

    wip
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- projects/esp32-ssd1306_128x32-lvgl/src/main.c
+++ projects/esp32-ssd1306_128x32-lvgl/src/main.c
@@ -16,78 +16,78 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app);
 
-static const char *now_str(void)
-{
-	static char buf[16]; /* ...HH:MM:SS.MMM */
-	uint32_t now = k_uptime_get_32();
-	unsigned int ms = now % MSEC_PER_SEC;
-	unsigned int s;
-	unsigned int min;
-	unsigned int h;
+/* static const char *now_str(void) */
+/* { */
+/* 	static char buf[16]; /\* ...HH:MM:SS.MMM *\/ */
+/* 	uint32_t now = k_uptime_get_32(); */
+/* 	unsigned int ms = now % MSEC_PER_SEC; */
+/* 	unsigned int s; */
+/* 	unsigned int min; */
+/* 	unsigned int h; */
 
-	now /= MSEC_PER_SEC;
-	s = now % 60U;
-	now /= 60U;
-	min = now % 60U;
-	now /= 60U;
-	h = now;
+/* 	now /= MSEC_PER_SEC; */
+/* 	s = now % 60U; */
+/* 	now /= 60U; */
+/* 	min = now % 60U; */
+/* 	now /= 60U; */
+/* 	h = now; */
 
-	snprintf(buf, sizeof(buf), "%u:%02u:%02u.%03u",
-		 h, min, s, ms);
-	return buf;
-}
+/* 	snprintf(buf, sizeof(buf), "%u:%02u:%02u.%03u", */
+/* 		 h, min, s, ms); */
+/* 	return buf; */
+/* } */
 
-static int process_mpu6050(const struct device *dev)
-{
-	struct sensor_value temperature;
-	struct sensor_value accel[3];
-	struct sensor_value gyro[3];
-	int rc = sensor_sample_fetch(dev);
+/* static int process_mpu6050(const struct device *dev) */
+/* { */
+/* 	struct sensor_value temperature; */
+/* 	struct sensor_value accel[3]; */
+/* 	struct sensor_value gyro[3]; */
+/* 	int rc = sensor_sample_fetch(dev); */
 
-	if (rc == 0) {
-		rc = sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ,
-					accel);
-	}
-	if (rc == 0) {
-		rc = sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ,
-					gyro);
-	}
-	if (rc == 0) {
-		rc = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP,
-					&temperature);
-	}
-	if (rc == 0) {
-		printf("[%s]:%g Cel\n"
-		       "  accel %f %f %f m/s/s\n"
-		       "  gyro  %f %f %f rad/s\n",
-		       now_str(),
-		       sensor_value_to_double(&temperature),
-		       sensor_value_to_double(&accel[0]),
-		       sensor_value_to_double(&accel[1]),
-		       sensor_value_to_double(&accel[2]),
-		       sensor_value_to_double(&gyro[0]),
-		       sensor_value_to_double(&gyro[1]),
-		       sensor_value_to_double(&gyro[2]));
-	} else {
-		printf("sample fetch/get failed: %d\n", rc);
-	}
+/* 	if (rc == 0) { */
+/* 		rc = sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, */
+/* 					accel); */
+/* 	} */
+/* 	if (rc == 0) { */
+/* 		rc = sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, */
+/* 					gyro); */
+/* 	} */
+/* 	if (rc == 0) { */
+/* 		rc = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, */
+/* 					&temperature); */
+/* 	} */
+/* 	if (rc == 0) { */
+/* 		printf("[%s]:%g Cel\n" */
+/* 		       "  accel %f %f %f m/s/s\n" */
+/* 		       "  gyro  %f %f %f rad/s\n", */
+/* 		       now_str(), */
+/* 		       sensor_value_to_double(&temperature), */
+/* 		       sensor_value_to_double(&accel[0]), */
+/* 		       sensor_value_to_double(&accel[1]), */
+/* 		       sensor_value_to_double(&accel[2]), */
+/* 		       sensor_value_to_double(&gyro[0]), */
+/* 		       sensor_value_to_double(&gyro[1]), */
+/* 		       sensor_value_to_double(&gyro[2])); */
+/* 	} else { */
+/* 		printf("sample fetch/get failed: %d\n", rc); */
+/* 	} */
 
-	return rc;
-}
+/* 	return rc; */
+/* } */
 
-static struct sensor_trigger trigger;
+/* static struct sensor_trigger trigger; */
 
-static void handle_mpu6050_drdy(const struct device *dev,
-				struct sensor_trigger *trig)
-{
-	int rc = process_mpu6050(dev);
+/* static void handle_mpu6050_drdy(const struct device *dev, */
+/* 				struct sensor_trigger *trig) */
+/* { */
+/* 	int rc = process_mpu6050(dev); */
 
-	if (rc != 0) {
-		printf("cancelling trigger due to failure: %d\n", rc);
-		(void)sensor_trigger_set(dev, trig, NULL);
-		return;
-	}
-}
+/* 	if (rc != 0) { */
+/* 		printf("cancelling trigger due to failure: %d\n", rc); */
+/* 		(void)sensor_trigger_set(dev, trig, NULL); */
+/* 		return; */
+/* 	} */
+/* } */
 
 void main(void)
 {
@@ -96,30 +96,32 @@ void main(void)
 	const struct device *display_dev;
 	lv_obj_t *hello_world_label;
 	lv_obj_t *count_label;
-	const char *const label = DT_LABEL(DT_INST(0, invensense_mpu6050));
-	const struct device *mpu6050 = device_get_binding(label);
 
-	trigger = (struct sensor_trigger) {
-		.type = SENSOR_TRIG_DATA_READY,
-		.chan = SENSOR_CHAN_ALL,
-	};
-	if (sensor_trigger_set(mpu6050, &trigger,
-			       handle_mpu6050_drdy) < 0) {
-		printf("Cannot configure trigger\n");
-		return;
-	};
-	printk("Configured for triggered sampling.\n");
+	/* const char *const label = DT_LABEL(DT_INST(0, invensense_mpu6050)); */
+	/* const struct device *mpu6050 = device_get_binding(label); */
+
+	/* trigger = (struct sensor_trigger) { */
+	/* 	.type = SENSOR_TRIG_DATA_READY, */
+	/* 	.chan = SENSOR_CHAN_ALL, */
+	/* }; */
+	/* if (sensor_trigger_set(mpu6050, &trigger, */
+	/* 		       handle_mpu6050_drdy) < 0) { */
+	/* 	printf("Cannot configure trigger\n"); */
+	/* 	return; */
+	/* }; */
+	/* printk("Configured for triggered sampling.\n"); */
+
+
+	/* while (!IS_ENABLED(CONFIG_MPU6050_TRIGGER)) { */
+	/* 	int rc = process_mpu6050(mpu6050); */
+
+	/* 	if (rc != 0) { */
+	/* 		break; */
+	/* 	} */
+	/* 	k_sleep(K_SECONDS(2)); */
+	/* } */
 
 	display_dev = device_get_binding(CONFIG_LVGL_DISPLAY_DEV_NAME);
-
-	while (!IS_ENABLED(CONFIG_MPU6050_TRIGGER)) {
-		int rc = process_mpu6050(mpu6050);
-
-		if (rc != 0) {
-			break;
-		}
-		k_sleep(K_SECONDS(2));
-	}
 
 	if (display_dev == NULL) {
 		LOG_ERR("device not found.  Aborting test.");
