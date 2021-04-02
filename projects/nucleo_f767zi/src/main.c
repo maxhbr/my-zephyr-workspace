2559b48214a16d8603ec71c76a79ef264731b45d
Maximilian Huber <gh@maxhbr.de>
2021-04-02 18:55:38 +0200

    progress
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- projects/nucleo_f767zi/src/main.c
+++ projects/nucleo_f767zi/src/main.c
@@ -6,6 +6,7 @@
 
 #include <device.h>
 #include <drivers/display.h>
+#include <drivers/sensor.h>
 #include <lvgl.h>
 #include <stdio.h>
 #include <string.h>
@@ -15,13 +16,130 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app);
 
+static const char *now_str(void)
+{
+	static char buf[16]; /* ...HH:MM:SS.MMM */
+	uint32_t now = k_uptime_get_32();
+	unsigned int ms = now % MSEC_PER_SEC;
+	unsigned int s;
+	unsigned int min;
+	unsigned int h;
+
+	now /= MSEC_PER_SEC;
+	s = now % 60U;
+	now /= 60U;
+	min = now % 60U;
+	now /= 60U;
+	h = now;
+
+	snprintf(buf, sizeof(buf), "%u:%02u:%02u.%03u",
+		 h, min, s, ms);
+	return buf;
+}
+
+static int process_mpu6050(const struct device *dev, lv_obj_t *label)
+{
+	struct sensor_value temperature;
+	struct sensor_value accel[3];
+	struct sensor_value gyro[3];
+	char gyro_str[40] = {0};
+	int rc = sensor_sample_fetch(dev);
+
+	if (rc == 0) {
+		rc = sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ,
+					accel);
+	}
+	if (rc == 0) {
+		rc = sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ,
+					gyro);
+	}
+	if (rc == 0) {
+		rc = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP,
+					&temperature);
+	}
+	if (rc == 0) {
+		printf("[%s]:%g Cel\n"
+		       "  accel %f %f %f m/s/s\n"
+		       "  gyro  %f %f %f rad/s\n",
+		       now_str(),
+		       sensor_value_to_double(&temperature),
+		       sensor_value_to_double(&accel[0]),
+		       sensor_value_to_double(&accel[1]),
+		       sensor_value_to_double(&accel[2]),
+		       sensor_value_to_double(&gyro[0]),
+		       sensor_value_to_double(&gyro[1]),
+		       sensor_value_to_double(&gyro[2]));
+		if (label) {
+			sprintf(gyro_str, "%f\n%f\n%f",
+					sensor_value_to_double(&gyro[0]),
+					sensor_value_to_double(&gyro[1]),
+					sensor_value_to_double(&gyro[2])
+					);
+			lv_label_set_text(label, gyro_str);
+		}
+	} else {
+		printf("sample fetch/get failed: %d\n", rc);
+	}
+
+	return rc;
+}
+
+#ifdef CONFIG_MPU6050_TRIGGER
+static struct sensor_trigger trigger;
+
+static void handle_mpu6050_drdy(const struct device *dev,
+				struct sensor_trigger *trig)
+{
+	int rc = process_mpu6050(dev, NULL);
+
+	if (rc != 0) {
+		printf("cancelling trigger due to failure: %d\n", rc);
+		(void)sensor_trigger_set(dev, trig, NULL);
+		return;
+	}
+}
+#endif /* CONFIG_MPU6050_TRIGGER */
+
+static
+const struct device *setup_mpu6050() {
+	const char *const label = DT_LABEL(DT_INST(0, invensense_mpu6050));
+	const struct device *mpu6050 = device_get_binding(label);
+
+	if (!mpu6050) {
+		printf("Failed to find sensor %s\n", label);
+		return mpu6050;
+	}
+
+#ifdef CONFIG_MPU6050_TRIGGER
+	trigger = (struct sensor_trigger) {
+		.type = SENSOR_TRIG_DATA_READY,
+		.chan = SENSOR_CHAN_ALL,
+	};
+	if (sensor_trigger_set(mpu6050, &trigger,
+			       handle_mpu6050_drdy) < 0) {
+		printf("Cannot configure trigger\n");
+		return;
+	};
+	printk("Configured for triggered sampling.\n");
+#endif
+
+	return mpu6050;
+}
+
 void main(void)
 {
 	uint32_t count = 0U;
 	char count_str[11] = {0};
 	const struct device *display_dev;
-	lv_obj_t *hello_world_label;
+	lv_obj_t *label;
 	lv_obj_t *count_label;
+
+
+	const struct device *mpu6050 = setup_mpu6050();
+	if (!mpu6050) {
+		return;
+	}
+
 
 	display_dev = device_get_binding(CONFIG_LVGL_DISPLAY_DEV_NAME);
 
@@ -36,24 +154,31 @@ void main(void)
 		hello_world_button = lv_btn_create(lv_scr_act(), NULL);
 		lv_obj_align(hello_world_button, NULL, LV_ALIGN_CENTER, 0, 0);
 		lv_btn_set_fit(hello_world_button, LV_FIT_TIGHT);
-		hello_world_label = lv_label_create(hello_world_button, NULL);
+		label = lv_label_create(hello_world_button, NULL);
 	} else {
-		hello_world_label = lv_label_create(lv_scr_act(), NULL);
+		label = lv_label_create(lv_scr_act(), NULL);
 	}
 
-	lv_label_set_text(hello_world_label, "Hello world!");
-	lv_obj_align(hello_world_label, NULL, LV_ALIGN_CENTER, 0, 0);
+	lv_label_set_text(label, "Hello world!");
+	lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, 0);
 
-	count_label = lv_label_create(lv_scr_act(), NULL);
-	lv_obj_align(count_label, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+    count_label = lv_label_create(lv_scr_act(), NULL);
+    lv_obj_align(count_label, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
 
 	lv_task_handler();
 	display_blanking_off(display_dev);
 
 	while (1) {
-		if ((count % 100) == 0U) {
-			sprintf(count_str, "%d", count/100U);
+		if ((count % 10) == 0U) {
+			sprintf(count_str, "%d", count/10U);
 			lv_label_set_text(count_label, count_str);
+			if (!IS_ENABLED(CONFIG_MPU6050_TRIGGER)) {
+				int rc = process_mpu6050(mpu6050, label);
+
+				if (rc != 0) {
+					break;
+				}
+			}
 		}
 		lv_task_handler();
 		k_sleep(K_MSEC(10));
