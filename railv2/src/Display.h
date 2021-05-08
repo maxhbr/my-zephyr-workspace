c5f132c493c479018dd286f056b7f21ebeaeaf51
Maximilian Huber <gh@maxhbr.de>
2021-05-08 12:06:30 +0200

    before tileview
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- railv2/src/Display.h
+++ railv2/src/Display.h
@@ -3,11 +3,14 @@
 
 #include <drivers/display.h>
 #include <lvgl.h>
+#include <map>
 
 class Display {
   void init_styles();
   lv_obj_t *tabview;
   void init_tabview(lv_obj_t *parent);
+  std::map<int, lv_obj_t *> index_to_tile;
+  void init_tileview(lv_obj_t *parent);
 
   const lv_font_t *font_title = &lv_font_montserrat_28;
   const lv_font_t *font_subtitle = &lv_font_montserrat_24;
@@ -22,6 +25,9 @@ public:
   lv_style_t style_box;
 
   lv_obj_t *make_tab(const char *title);
+  lv_obj_t *get_tile(int index) {
+    return index_to_tile.at(index);
+  };
   lv_obj_t *add_label(lv_obj_t *parent);
   lv_obj_t *add_panel(lv_obj_t *parent);
   lv_obj_t *add_button(lv_obj_t *parent, const char *label_text, int width, int heigth);
