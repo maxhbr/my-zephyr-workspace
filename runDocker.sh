e9a12c972a67f72ac12c1d1da1daa2457bc7064e
Maximilian Huber <gh@maxhbr.de>
2021-07-10 09:53:39 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- runDocker.sh
+++ runDocker.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-docker run -ti -p 5900:5900 -v "$(cd "$(dirname "$0")" && pwd)":/workdir zephyr-build:latest
+tag="docker.io/zephyrprojectrtos/zephyr-build:latest"
+
+# (
+#     cd docker-image/
+#     tag=zephyr-build:latest
+#     docker build -f Dockerfile.user --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t "$tag"
+# )
+
+docker run -ti -p 5900:5900 -v "$(cd "$(dirname "$0")" && pwd)":/workdir "$tag"
